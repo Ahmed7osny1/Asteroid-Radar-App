8b0a311d44ff0bb41a35ca0150de9982982abc69
Ahmed <Ahmedhosny6688@gmail.com>
2022-08-06 21:03:05 +0200

    Final commit

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -9,13 +9,24 @@ import com.udacity.asteroidradar.repository.AsteroidsRepository
 import com.udacity.asteroidradar.repository.PicturesOfDayRepository
 import kotlinx.coroutines.launch
 
+/**
+ * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
+ * allows data to survive configuration changes such as screen rotations. In addition, background
+ * work such as fetching network results can continue through configuration changes and deliver
+ * results after the new Fragment or Activity is available.
+ *
+ * @param application The application that this viewmodel is attached to, it's safe to hold a
+ * reference to applications across rotation since Application is never recreated during activty
+ * or fragment lifecycle events.
+ */
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val database = getDatabase(application)
     private val filter = MutableLiveData(AsteroidApiFilter.SHOW_SAVED)
     private val picturesOfDayRepository = PicturesOfDayRepository(database)
     private val asteroidsRepository = AsteroidsRepository(database)
-
-
+    /**
+     * init{} is called immediately when this ViewModel is created.
+     */
     init {
         viewModelScope.launch {
             picturesOfDayRepository.refreshPictureOfDay()
@@ -32,8 +43,10 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-
-    abstract class Factory(val app: Application) : ViewModelProvider.Factory {
+    /**
+     * Factory for constructing MainViewModel with parameter
+     */
+    class Factory(val app: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
@@ -50,17 +63,26 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     val navigateToSelectedProperty: MutableLiveData<Asteroid?>
         get() = _navigateToSelectedAsteroid
 
-
+    /**
+     * When the asteroid is clicked, set the [_navigateToSelectedAsteroid] [MutableLiveData]
+     * @param marsProperty The [MarsProperty] that was clicked on.
+     */
     fun displayAsteroidDetails(asteroid: Asteroid) {
         _navigateToSelectedAsteroid.value = asteroid
     }
 
-
+    /**
+     * After the navigation has taken place, make sure navigateToSelectedProperty is set to null
+     */
     fun displayAsteroidDetailsComplete() {
         _navigateToSelectedAsteroid.value = null
     }
 
-
+    /**
+     * Updates the data set filter for the web services by querying the data with the new filter
+     * by calling [getAsteroids]
+     * @param filter the [AsteroidApiFilter] that is sent as part of the web server request
+     */
     fun updateFilter(filter: AsteroidApiFilter) {
         this.filter.value = filter
     }
